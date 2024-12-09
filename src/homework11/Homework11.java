6eec6e6fd1ef104c9c4cc4a8a96df47fb3c1b149
zwk <alexanchik6@mail.ru>
2024-12-09 20:08:34 +0300

    second commit

--- src/homework11/Homework11.java
+++ src/homework11/Homework11.java
@@ -12,7 +12,10 @@ public class Homework11 {
         Random r = new Random();
         int hours=(r.nextInt(23) + 1);
         int minutes=(r.nextInt(59) + 1);
+        int hours1=(r.nextInt(23) + 1);
+        int minutes1=(r.nextInt(59) + 1);
         String time = hours + ":" + minutes;
+        String time1 = hours1 + ":" + minutes1;
         User user1 = new User("Grigoriy", "grisha2001@gmail.com", +3750000);
         User user2 = new User("Dmitry", "dima1998@gmail.com", +3751111);
         Exchanger<String> exchanger = new Exchanger<>();
@@ -24,7 +27,7 @@ public class Homework11 {
                 String message = null;
                 try {
                     message = exchanger.exchange("Hello from " + user1.getName());
-                    System.out.println("Grigoriy got message from " + user2.getNumber() + ", message: " + message + ", time the message was sent " + time);
+                    System.out.println("Grigoriy got message from " + user2.getNumber() + ", message: " + message + ", time the message was sent: " + time);
                 } catch (InterruptedException ignored) {
                 }
             });
@@ -33,7 +36,7 @@ public class Homework11 {
                 String message = null;
                 try {
                     message = exchanger.exchange("Hello from " + user2.getName());
-                    System.out.println("Dmitry got message from " + user1.getNumber() + ", message: " + message + ", time the message was sent " + time);
+                    System.out.println("Dmitry got message from " + user1.getNumber() + ", message: " + message + ", time the message was sent: " + time1);
                 } catch (InterruptedException ignored) {
                 }
             });
@@ -48,7 +51,7 @@ public class Homework11 {
                 String message = null;
                 try {
                     message = exchanger.exchange("Hello from " + user1.getName());
-                    System.out.println("Grigoriy got message from " + user2.getMail() + ", message: " + message + ", time the message was sent " + time);
+                    System.out.println("Grigoriy got message from " + user2.getMail() + ", message: " + message + ", time the message was sent: " + time);
                 } catch (InterruptedException ignored) {
                 }
             });
@@ -57,7 +60,7 @@ public class Homework11 {
                 String message = null;
                 try {
                     message = exchanger.exchange("Hello from " + user2.getName());
-                    System.out.println("Dmitry got message from " + user1.getMail() + ", message: " + message + ", time the message was sent " + time);
+                    System.out.println("Dmitry got message from " + user1.getMail() + ", message: " + message + ", time the message was sent: " + time1);
                 } catch (InterruptedException ignored) {
                 }
             });
